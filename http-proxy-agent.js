7c892f8fbdd1e7ab4ae1e07db2c96f126896604e
Nathan Rajlich <nathan@tootallnate.net>
2013-09-10 23:50:25 -0700

    http-proxy-agent: use "extend" module

--- http-proxy-agent.js
+++ http-proxy-agent.js
@@ -6,6 +6,7 @@
 var net = require('net');
 var tls = require('tls');
 var url = require('url');
+var extend = require('extend');
 var Agent = require('agent-base');
 var inherits = require('util').inherits;
 
@@ -28,7 +29,7 @@ function HttpProxyAgent (opts) {
   if (!opts) throw new Error('an HTTP(S) proxy server `host` and `port` must be specified!');
   Agent.call(this, connect);
 
-  var proxy = clone(opts, {});
+  var proxy = extend({}, opts);
 
   // if `true`, then connect to the proxy server over TLS. defaults to `false`.
   this.secureProxy = proxy.protocol ? proxy.protocol == 'https:' : false;
@@ -49,13 +50,24 @@ function HttpProxyAgent (opts) {
 inherits(HttpProxyAgent, Agent);
 
 /**
+ * Default options for the "connect" opts object.
+ */
+
+var defaults = { port: 80 };
+
+/**
  * Called when the node-core HTTP client library is creating a new HTTP request.
  *
  * @api public
  */
 
-function connect (req, opts, fn) {
-  if (null == opts.port) opts.port = 80;
+function connect (req, _opts, fn) {
+
+  var proxy = this.proxy;
+  var secureProxy = this.secureProxy;
+
+  // these `opts` are the connect options to connect to the destination endpoint
+  var opts = extend({}, proxy, defaults, _opts);
 
   // change the `http.ClientRequest` instance's "path" field
   // to the absolute path of the URL that will be requested
@@ -68,22 +80,17 @@ function connect (req, opts, fn) {
   req.path = absolute;
 
   // inject the `Proxy-Authorization` header if necessary
-  var auth = this.proxy.auth;
+  var auth = proxy.auth;
   if (auth) {
     req.setHeader('Proxy-Authorization', 'Basic ' + new Buffer(auth).toString('base64'));
   }
 
   var socket;
-  if (this.secureProxy) {
-    socket = tls.connect(this.proxy);
+  if (secureProxy) {
+    socket = tls.connect(proxy);
   } else {
-    socket = net.connect(this.proxy);
+    socket = net.connect(proxy);
   }
 
   fn(null, socket);
 };
-
-function clone (src, dest) {
-  for (var i in src) dest[i] = src[i];
-  return dest;
-}
