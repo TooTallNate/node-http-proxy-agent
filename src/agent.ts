a5c36df6245d833c65dc1a9bdfb4bbf23dec81ed
Nathan Rajlich <nathan@tootallnate.net>
2019-12-12 19:30:22 -0800

    Fix most `@ts-ignore` and most lint

--- src/agent.ts
+++ src/agent.ts
@@ -2,10 +2,20 @@ import net from 'net';
 import tls from 'tls';
 import url from 'url';
 import createDebug from 'debug';
-import { HttpProxyAgentOptions } from './';
 import { Agent, ClientRequest, RequestOptions } from 'agent-base';
+import { HttpProxyAgentOptions } from '.';
 
 const debug = createDebug('http-proxy-agent');
+
+interface HttpProxyAgentClientRequest extends ClientRequest {
+	path: string;
+	output?: string[];
+	outputData?: {
+		data: string;
+	}[];
+	_header?: string | null;
+	_implicitHeader(): void;
+}
 
 /**
  * The `HttpProxyAgent` implements an HTTP Agent subclass that connects
@@ -42,6 +52,9 @@ export default class HttpProxyAgent extends Agent {
 
 		// Prefer `hostname` over `host`, and set the `port` if needed.
 		proxy.host = proxy.hostname || proxy.host;
+
+		// XXX: For some reason, TS types `proxy.port` as `never` and
+		// doesn't allow assignment. Ignore for now.
 		// @ts-ignore
 		proxy.port = +proxy.port || (this.secureProxy ? 443 : 80);
 
@@ -63,15 +76,26 @@ export default class HttpProxyAgent extends Agent {
 	 *
 	 * @api protected
 	 */
-	callback(req: ClientRequest, opts: RequestOptions): net.Socket {
+	callback(
+		req: HttpProxyAgentClientRequest,
+		opts: RequestOptions
+	): net.Socket {
 		const { proxy, secureProxy } = this;
+		const parsed = url.parse(req.path);
 
-		const parsed = url.parse(req.path) as any;
-		if (parsed.protocol == null) parsed.protocol = 'http:';
-		if (parsed.hostname == null)
+		if (!parsed.protocol) {
+			parsed.protocol = 'http:';
+		}
+
+		if (!parsed.hostname) {
 			parsed.hostname = opts.hostname || opts.host || null;
-		if (parsed.port == null) parsed.port = opts.port;
-		if (parsed.port == 80) {
+		}
+
+		if (parsed.port == null && typeof opts.port) {
+			parsed.port = String(opts.port);
+		}
+
+		if (parsed.port === '80') {
 			// if port is 80, then we can remove the port so that the
 			// ":80" portion is not on the produced URL
 			delete parsed.port;
@@ -79,9 +103,6 @@ export default class HttpProxyAgent extends Agent {
 
 		// Change the `http.ClientRequest` instance's "path" field
 		// to the absolute path of the URL that will be requested.
-		//
-		// XXX: Ignore because in `@types/node` the `path` property is `readonly`
-		// @ts-ignore
 		req.path = url.format(parsed);
 
 		// Inject the `Proxy-Authorization` header if necessary.
@@ -105,40 +126,30 @@ export default class HttpProxyAgent extends Agent {
 		// At this point, the http ClientRequest's internal `_header` field
 		// might have already been set. If this is the case then we'll need
 		// to re-generate the string since we just changed the `req.path`.
-		// @ts-ignore
 		if (req._header) {
+			let first: string;
+			let endOfHeaders: number;
 			debug('Regenerating stored HTTP header string for request');
-			// @ts-ignore
 			req._header = null;
-			// @ts-ignore
 			req._implicitHeader();
-			// @ts-ignore
 			if (req.output && req.output.length > 0) {
 				// Node < 12
 				debug(
 					'Patching connection write() output buffer with updated header'
 				);
-				// @ts-ignore
-				var first = req.output[0];
-				var endOfHeaders = first.indexOf('\r\n\r\n') + 4;
-				// @ts-ignore
+				first = req.output[0];
+				endOfHeaders = first.indexOf('\r\n\r\n') + 4;
 				req.output[0] = req._header + first.substring(endOfHeaders);
-				// @ts-ignore
 				debug('Output buffer: %o', req.output);
-				// @ts-ignore
 			} else if (req.outputData && req.outputData.length > 0) {
 				// Node >= 12
 				debug(
 					'Patching connection write() output buffer with updated header'
 				);
-				// @ts-ignore
-				var first = req.outputData[0].data;
-				var endOfHeaders = first.indexOf('\r\n\r\n') + 4;
-				// @ts-ignore
+				first = req.outputData[0].data;
+				endOfHeaders = first.indexOf('\r\n\r\n') + 4;
 				req.outputData[0].data =
-					// @ts-ignore
 					req._header + first.substring(endOfHeaders);
-				// @ts-ignore
 				debug('Output buffer: %o', req.outputData[0].data);
 			}
 		}
