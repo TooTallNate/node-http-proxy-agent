cab60b0a678dbf80648fa0650ac68525efc5b26e
Nathan Rajlich <n@n8.io>
2020-01-23 11:52:04 -0800

    Refactor to TypeScript (#14)
    * Refactor to TypeScript
    
    * Fix most `@ts-ignore` and most lint
    
    * Remove `jest` for now
    
    * Better `HttpProxyAgentOptions` definition and fix last `@ts-ignore`
    
    * Support `secureProxy` option
    
    * Prettier
    
    * Fix `type`
    
    * Use `keyof`

--- test/test.js
+++ test/test.js
@@ -2,27 +2,37 @@
  * Module dependencies.
  */
 
-var fs = require('fs');
-var url = require('url');
-var http = require('http');
-var https = require('https');
-var assert = require('assert');
-var Proxy = require('proxy');
-var HttpProxyAgent = require('../');
+const fs = require('fs');
+const net = require('net');
+const url = require('url');
+const http = require('http');
+const https = require('https');
+const assert = require('assert');
+const Proxy = require('proxy');
+const { Agent } = require('agent-base');
+const { HttpProxyAgent } = require('../');
+
+const sleep = n => new Promise(r => setTimeout(r, n));
 
 describe('HttpProxyAgent', function() {
-	var server;
-	var serverPort;
+	let server;
+	let serverPort;
 
-	var proxy;
-	var proxyPort;
+	let proxy;
+	let proxyPort;
 
-	var sslProxy;
-	var sslProxyPort;
+	let sslProxy;
+	let sslProxyPort;
 
 	before(function(done) {
 		// setup HTTP proxy server
 		proxy = Proxy();
+		/*
+		proxy = net.createServer((socket) => {
+			socket.setEncoding('utf8');
+			socket.on('data', console.log);
+		});
+		*/
 		proxy.listen(function() {
 			proxyPort = proxy.address().port;
 			done();
@@ -44,9 +54,9 @@ describe('HttpProxyAgent', function() {
 
 	before(function(done) {
 		// setup SSL HTTP proxy server
-		var options = {
-			key: fs.readFileSync(__dirname + '/ssl-cert-snakeoil.key'),
-			cert: fs.readFileSync(__dirname + '/ssl-cert-snakeoil.pem')
+		let options = {
+			key: fs.readFileSync(`${__dirname}/ssl-cert-snakeoil.key`),
+			cert: fs.readFileSync(`${__dirname}/ssl-cert-snakeoil.pem`)
 		};
 		sslProxy = Proxy(https.createServer(options));
 		sslProxy.listen(function() {
@@ -84,44 +94,51 @@ describe('HttpProxyAgent', function() {
 			});
 		});
 		it('should accept a "string" proxy argument', function() {
-			var agent = new HttpProxyAgent('http://127.0.0.1:' + proxyPort);
+			let agent = new HttpProxyAgent(`http://127.0.0.1:${proxyPort}`);
 			assert.equal('127.0.0.1', agent.proxy.host);
 			assert.equal(proxyPort, agent.proxy.port);
 		});
 		it('should accept a `url.parse()` result object argument', function() {
-			var opts = url.parse('http://127.0.0.1:' + proxyPort);
-			var agent = new HttpProxyAgent(opts);
+			let opts = url.parse(`http://127.0.0.1:${proxyPort}`);
+			let agent = new HttpProxyAgent(opts);
 			assert.equal('127.0.0.1', agent.proxy.host);
 			assert.equal(proxyPort, agent.proxy.port);
 		});
 		it('should set a `defaultPort` property', function() {
-			var opts = url.parse('http://127.0.0.1:' + proxyPort);
-			var agent = new HttpProxyAgent(opts);
+			let opts = url.parse(`http://127.0.0.1:${proxyPort}`);
+			let agent = new HttpProxyAgent(opts);
 			assert.equal(80, agent.defaultPort);
 		});
 		describe('secureProxy', function() {
 			it('should default to `false`', function() {
-				var agent = new HttpProxyAgent({ port: proxyPort });
+				let agent = new HttpProxyAgent({ port: proxyPort });
 				assert.equal(false, agent.secureProxy);
 			});
 			it('should be `false` when "http:" protocol is used', function() {
-				var agent = new HttpProxyAgent({
+				let agent = new HttpProxyAgent({
 					port: proxyPort,
 					protocol: 'http:'
 				});
 				assert.equal(false, agent.secureProxy);
 			});
 			it('should be `true` when "https:" protocol is used', function() {
-				var agent = new HttpProxyAgent({
+				let agent = new HttpProxyAgent({
 					port: proxyPort,
 					protocol: 'https:'
 				});
 				assert.equal(true, agent.secureProxy);
 			});
 			it('should be `true` when "https" protocol is used', function() {
-				var agent = new HttpProxyAgent({
+				let agent = new HttpProxyAgent({
 					port: proxyPort,
 					protocol: 'https'
+				});
+				assert.equal(true, agent.secureProxy);
+			});
+			it('should support a `defaultPort` option', function() {
+				let agent = new HttpProxyAgent({
+					port: proxyPort,
+					secureProxy: true
 				});
 				assert.equal(true, agent.secureProxy);
 			});
@@ -135,24 +152,24 @@ describe('HttpProxyAgent', function() {
 				res.end(JSON.stringify(req.headers));
 			});
 
-			var proxy =
+			let proxy =
 				process.env.HTTP_PROXY ||
 				process.env.http_proxy ||
-				'http://127.0.0.1:' + proxyPort;
-			var agent = new HttpProxyAgent(proxy);
+				`http://127.0.0.1:${proxyPort}`;
+			let agent = new HttpProxyAgent(proxy);
 
-			var opts = url.parse('http://127.0.0.1:' + serverPort);
+			let opts = url.parse(`http://127.0.0.1:${serverPort}`);
 			opts.agent = agent;
 
 			http.get(opts, function(res) {
-				var data = '';
+				let data = '';
 				res.setEncoding('utf8');
 				res.on('data', function(b) {
 					data += b;
 				});
 				res.on('end', function() {
 					data = JSON.parse(data);
-					assert.equal('127.0.0.1:' + serverPort, data.host);
+					assert.equal(`127.0.0.1:${serverPort}`, data.host);
 					assert('via' in data);
 					done();
 				});
@@ -164,27 +181,27 @@ describe('HttpProxyAgent', function() {
 				res.end(JSON.stringify(req.headers));
 			});
 
-			var proxy =
+			let proxy =
 				process.env.HTTPS_PROXY ||
 				process.env.https_proxy ||
-				'https://127.0.0.1:' + sslProxyPort;
+				`https://127.0.0.1:${sslProxyPort}`;
 			proxy = url.parse(proxy);
 			proxy.rejectUnauthorized = false;
-			var agent = new HttpProxyAgent(proxy);
+			let agent = new HttpProxyAgent(proxy);
 			assert.equal(true, agent.secureProxy);
 
-			var opts = url.parse('http://127.0.0.1:' + serverPort);
+			let opts = url.parse(`http://127.0.0.1:${serverPort}`);
 			opts.agent = agent;
 
 			http.get(opts, function(res) {
-				var data = '';
+				let data = '';
 				res.setEncoding('utf8');
 				res.on('data', function(b) {
 					data += b;
 				});
 				res.on('end', function() {
 					data = JSON.parse(data);
-					assert.equal('127.0.0.1:' + serverPort, data.host);
+					assert.equal(`127.0.0.1:${serverPort}`, data.host);
 					assert('via' in data);
 					done();
 				});
@@ -200,19 +217,19 @@ describe('HttpProxyAgent', function() {
 				);
 			});
 
-			var proxy =
+			let proxy =
 				process.env.HTTP_PROXY ||
 				process.env.http_proxy ||
-				'http://127.0.0.1:' + proxyPort;
-			var agent = new HttpProxyAgent(proxy);
+				`http://127.0.0.1:${proxyPort}`;
+			let agent = new HttpProxyAgent(proxy);
 
-			var opts = url.parse(
-				'http://127.0.0.1:' + serverPort + '/test?foo=bar&1=2'
+			let opts = url.parse(
+				`http://127.0.0.1:${serverPort}/test?foo=bar&1=2`
 			);
 			opts.agent = agent;
 
 			http.get(opts, function(res) {
-				var data = '';
+				let data = '';
 				res.setEncoding('utf8');
 				res.on('data', function(b) {
 					data += b;
@@ -231,13 +248,13 @@ describe('HttpProxyAgent', function() {
 				fn(null, false);
 			};
 
-			var proxyUri =
+			let proxyUri =
 				process.env.HTTP_PROXY ||
 				process.env.http_proxy ||
-				'http://127.0.0.1:' + proxyPort;
-			var agent = new HttpProxyAgent(proxyUri);
+				`http://127.0.0.1:${proxyPort}`;
+			let agent = new HttpProxyAgent(proxyUri);
 
-			var opts = {};
+			let opts = {};
 			// `host` and `port` don't really matter since the proxy will reject anyways
 			opts.host = '127.0.0.1';
 			opts.port = 80;
@@ -265,26 +282,26 @@ describe('HttpProxyAgent', function() {
 				res.end(JSON.stringify(req.headers));
 			});
 
-			var proxyUri =
+			let proxyUri =
 				process.env.HTTP_PROXY ||
 				process.env.http_proxy ||
-				'http://127.0.0.1:' + proxyPort;
-			var proxyOpts = url.parse(proxyUri);
+				`http://127.0.0.1:${proxyPort}`;
+			let proxyOpts = url.parse(proxyUri);
 			proxyOpts.auth = 'foo:bar';
-			var agent = new HttpProxyAgent(proxyOpts);
+			let agent = new HttpProxyAgent(proxyOpts);
 
-			var opts = url.parse('http://127.0.0.1:' + serverPort);
+			let opts = url.parse(`http://127.0.0.1:${serverPort}`);
 			opts.agent = agent;
 
 			http.get(opts, function(res) {
-				var data = '';
+				let data = '';
 				res.setEncoding('utf8');
 				res.on('data', function(b) {
 					data += b;
 				});
 				res.on('end', function() {
 					data = JSON.parse(data);
-					assert.equal('127.0.0.1:' + serverPort, data.host);
+					assert.equal(`127.0.0.1:${serverPort}`, data.host);
 					assert('via' in data);
 					delete proxy.authenticate;
 					done();
@@ -293,13 +310,13 @@ describe('HttpProxyAgent', function() {
 		});
 		it('should emit an "error" event on the `http.ClientRequest` if the proxy does not exist', function(done) {
 			// port 4 is a reserved, but "unassigned" port
-			var proxyUri = 'http://127.0.0.1:4';
-			var agent = new HttpProxyAgent(proxyUri);
+			let proxyUri = 'http://127.0.0.1:4';
+			let agent = new HttpProxyAgent(proxyUri);
 
-			var opts = url.parse('http://nodejs.org');
+			let opts = url.parse('http://nodejs.org');
 			opts.agent = agent;
 
-			var req = http.get(opts);
+			let req = http.get(opts);
 			req.once('error', function(err) {
 				assert.equal('ECONNREFUSED', err.code);
 				req.abort();
@@ -312,28 +329,31 @@ describe('HttpProxyAgent', function() {
 				res.end(JSON.stringify(req.url));
 			});
 
-			var proxy =
+			let proxy =
 				process.env.HTTP_PROXY ||
 				process.env.http_proxy ||
-				'http://127.0.0.1:' + proxyPort;
-			var agent = new HttpProxyAgent(proxy);
+				`http://127.0.0.1:${proxyPort}`;
+			let httpProxyAgent = new HttpProxyAgent(proxy);
 
-			var opts = url.parse('http://127.0.0.1:' + serverPort + '/test');
-			opts.agent = agent;
+			// Defer the "connect()" function logic, since calling `req.end()`
+			// before the socket is returned causes the HTTP header to be
+			// generated *before* `HttpProxyAgent` can patch the `req.path`
+			// property, making the header incorrect.
+			const sleepAgent = new Agent((req, opts) => {
+				assert.equal(opts.secureEndpoint, false);
+				assert.equal(opts.protocol, 'http:');
+				assert(!req._header);
+				return sleep(10).then(() => {
+					assert.equal(typeof req._header, 'string');
+					return httpProxyAgent;
+				});
+			});
 
-			// defer the "connect()" function logic, since calling .end() before the
-			// "socket" event can cause weirdness since the HTTP header will have been
-			// cached and the HttpProxyAgent `req.path` patches won't be respected
-			var callback = agent.callback;
-			agent.callback = function(req, opts, fn) {
-				setTimeout(function() {
-					agent.callback = callback;
-					agent.callback(req, opts, fn);
-				}, 10);
-			};
+			const opts = url.parse(`http://127.0.0.1:${serverPort}/test`);
+			opts.agent = sleepAgent;
 
 			http.get(opts, function(res) {
-				var data = '';
+				let data = '';
 				res.setEncoding('utf8');
 				res.on('data', function(b) {
 					data += b;
@@ -349,17 +369,17 @@ describe('HttpProxyAgent', function() {
 			server.once('request', function(req, res) {
 				res.end(JSON.stringify(req.headers));
 			});
-			var proxy =
+			let proxy =
 				process.env.HTTP_PROXY ||
 				process.env.http_proxy ||
-				'http://127.0.0.1:' + proxyPort;
+				`http://127.0.0.1:${proxyPort}`;
 			proxy = url.parse(proxy);
-			var agent = new HttpProxyAgent(proxy);
+			let agent = new HttpProxyAgent(proxy);
 			agent.defaultPort = serverPort;
-			var opts = url.parse('http://127.0.0.1:' + serverPort);
+			let opts = url.parse(`http://127.0.0.1:${serverPort}`);
 			opts.agent = agent;
 			http.get(opts, function(res) {
-				var data = '';
+				let data = '';
 				res.setEncoding('utf8');
 				res.on('data', function(b) {
 					data += b;
